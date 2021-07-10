4b21a1b9c1a95b19e2e26fcf987f0328506bdd19
chirs <chirs@chirs.com>
2021-07-09 22:53:26 -0700

    basic layout setup with necessory dependencies

---
+++ src/Components/index.js
@@ -0,0 +1,4 @@
+export { default as CryptoCard } from "./CryptoCard";
+export { default as CryptoCardView } from "./CryptoCardView";
+export { default as CryptoListView } from "./CryptoListView";
+export { default as CryptoContainer } from "./CryptoContainer";
